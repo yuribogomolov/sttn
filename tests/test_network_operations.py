afe5c16a5b69857ed672a1c3d848cc29103834a1
Yuri Bogomolov <iura@tut.by>
2021-03-28 20:30:39 -0700

    Add agg_adjacent_edges test.

--- tests/test_network_operations.py
+++ tests/test_network_operations.py
@@ -31,3 +31,20 @@ def test_agg_parallel_edges_with_key():
 
     expected = pd.DataFrame(data={'origin': [1, 1, 2, 2], 'destination': [2, 2, 1, 2], 'key': [1, 2, 1, 1], 'value': [9, 2, 4, 16]})
     assert_frame_equal(aggregated.edges, expected)
+
+
+def test_agg_adjacent_edges():
+    # outgoing = True, include_cycles = True
+    edges_aggregated = stn.agg_adjacent_edges(aggs={'value': 'sum'})
+    expected_outgoing = pd.DataFrame(data={'origin': [1, 2], 'value': [11, 20]}).set_index('origin')
+    assert_frame_equal(edges_aggregated, expected_outgoing)
+
+    # outgoing = False, include_cycles = True
+    incoming_aggregated = stn.agg_adjacent_edges(aggs={'value': 'sum'}, outgoing=False)
+    expected_incoming = pd.DataFrame(data={'destination': [1, 2], 'value': [4, 27]}).set_index('destination')
+    assert_frame_equal(incoming_aggregated, expected_incoming)
+
+    # outgoing = True, include_cycles = False
+    aggregated_no_cycle = stn.agg_adjacent_edges(aggs={'value': 'sum'}, include_cycles=False)
+    expected_no_cycle = pd.DataFrame(data={'origin': [1, 2], 'value': [11, 4]}).set_index('origin')
+    assert_frame_equal(aggregated_no_cycle, expected_no_cycle)
