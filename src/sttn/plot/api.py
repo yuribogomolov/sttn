4b37b4a9a0231ed226f7adadfb56bf4b7fd672cb
Yuri Bogomolov <iura@tut.by>
2022-10-08 19:58:32 -0700

    Add a time range filter.

--- src/sttn/plot/api.py
+++ src/sttn/plot/api.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from sttn.network import SpatioTemporalNetwork
 from sttn.utils import get_edges_with_centroids
 from keplergl import KeplerGl
@@ -9,7 +11,7 @@ DEFAULT_MAP_HEIGHT = 600
 
 
 def choropleth(data: SpatioTemporalNetwork, node_layers: Optional[List[str]] = None, include_edges: bool = False,
-               edge_size: Optional[str] = None) -> KeplerGl:
+               edge_size_column: Optional[str] = None) -> KeplerGl:
     map_conf = MapConfig(data_id='nodes')
     nodes = data.nodes
 
@@ -20,12 +22,31 @@ def choropleth(data: SpatioTemporalNetwork, node_layers: Optional[List[str]] = N
 
     if include_edges:
         edges = get_edges_with_centroids(data)
-        map_conf.with_data_id('edges').add_arc_layer(label=f"edge: {edge_size}", origin_lat="lat_from",
+        map_conf.with_data_id('edges').add_arc_layer(label=f"edge: {edge_size_column}", origin_lat="lat_from",
                                                      origin_lng="long_from",
                                                      destination_lat="lat_to", destination_lng="long_to",
-                                                     size_column=edge_size)
+                                                     size_column=edge_size_column)
         map_data['edges'] = edges
 
+    output_map = KeplerGl(height=DEFAULT_MAP_HEIGHT, data=map_data, config=map_conf.to_dict())
+    return output_map
+
+
+def transaction_time_motion(data: SpatioTemporalNetwork, time_column: str,
+                            edge_size_column: str) -> KeplerGl:
+    map_conf = MapConfig(data_id='edges')
+    edges = get_edges_with_centroids(data)
+    map_conf.add_arc_layer(label=f"edge: {edge_size_column}", origin_lat="lat_from",
+                           origin_lng="long_from",
+                           destination_lat="lat_to", destination_lng="long_to",
+                           size_column=edge_size_column)
+
+    window_start = data.edges[time_column].min()
+    window_end = data.edges[time_column].max()
+    window = (window_start.value // 1000000, window_end.value // 1000000)
+
+    map_conf.add_time_range_filter(time_column=time_column, window=window, y_axis_column=edge_size_column)
+    map_data = {'edges': edges}
     output_map = KeplerGl(height=DEFAULT_MAP_HEIGHT, data=map_data, config=map_conf.to_dict())
     return output_map
 
--- src/sttn/plot/keplergl/map.py
+++ src/sttn/plot/keplergl/map.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 from sttn.plot.keplergl.layer import ArcLayer, GeoMapLayer
+from sttn.plot.keplergl.filter import TimeRangeFilter
 
 
 class MapConfig:
     def __init__(self, data_id: str):
         self.layers = []
+        self.filters = []
         self._data_id = data_id
 
     def with_data_id(self, data_id) -> MapConfig:
@@ -25,7 +27,14 @@ class MapConfig:
         self.layers.append(layer)
         return self
 
+    def add_time_range_filter(self, time_column: str, window: Tuple[int, int], y_axis_column: str) -> MapConfig:
+        range_filter = TimeRangeFilter(data_id=self._data_id, time_column=time_column, window=window,
+                                       y_axis_column=y_axis_column)
+        self.filters.append(range_filter)
+        return self
+
     def to_dict(self) -> Dict[str, Any]:
         layer_configs = [layer.get_config() for layer in self.layers]
-        return {'version': 'v1', 'config': {'visState': {'filters': [],
+        filter_configs = [map_filter.get_config() for map_filter in self.filters]
+        return {'version': 'v1', 'config': {'visState': {'filters': filter_configs,
                                                          'layers': layer_configs}}}
