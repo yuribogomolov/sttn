2c39e73b8d1189305de146f0ec09f16035c6b1b9
BreckoD <43202795+BreckoD@users.noreply.github.com>
2024-01-26 18:20:37 +0100

    Added columns description and typing information for function arguments in restaurant.py

--- src/sttn/data/restaurants.py
+++ src/sttn/data/restaurants.py
@@ -6,12 +6,12 @@ from sttn import network
 from .data_provider import DataProvider
 
 class RestaurantInspectionDataProvider(DataProvider):
-    """New York City Restaurant Inspections data provider, builds a network where nodes represent restaurants
+    """New York City Restaurant Inspections data provider builds a network where nodes represent restaurants (establishments)
      and every edge represents an inspection where the origin and destination point to the node
      where the inspection happened."""
   
     @staticmethod
-    def get_data(self, from_date, to_date) -> network.SpatioTemporalNetwork:
+    def get_data(self, from_date: str, to_date: str) -> network.SpatioTemporalNetwork:
         """
         Retrieves New York City Restaurant Inspections data
 
@@ -26,11 +26,44 @@ class RestaurantInspectionDataProvider(DataProvider):
         Returns:
             SpatioTemporalNetwork: An STTN network where nodes represent New York City restaurants
                 and edges represent inspections.
+            
+            The nodes dataframe contains the following columns:
+                'id' (int64) - restaurant id, also referred as CAMIS
+                'geometry' (geometry) - geometry object for the restaurant location
+                'NAME' (str) - restaurant name, also referred as DBA ('doing bussiness as')
+                'BORO' (str) - restaurant location borough
+                'BUILDING' (str) - restaurant location building code
+                'STREET' (str) - restaurant location street name
+                'ZIPCODE' (float64) - restaurant location zipcode
+                'PHONE' (str) - restaurant phone number
+                'CUISINE_DESCRIPTION' (str) - restaurant cuisine description
+                'COUNCIL_DISTRICT' (float64) - restaurant council district number
+                'CENSUS_TRACT' (float64) - restaurant census tract number
+                'COMMUNITY_BOARD' (float64) - restaurant community board number
+                'BIN' (float64) - restaurant Building Identification Number
+                'BBL' (float64) - restaurant Borough-Block-and-Lot number
+                'NTA' (str) - restaurant Neighborhood Tabulation Area code
+                
+
+            The edges dataframe contains the following columns:
+                'origin' (int64) - inspected restaurant id
+                'destination' (int64) - inspected restaurant id
+                'INSPECTION_DATE' (datetime64[ns]) - inspection date
+                'ACTION' (str) - action associated with each restaurant inspection
+                'VIOLATION_CODE' (str) - violation code associated with an restaurant inspection
+                'VIOLATION_DESCRIPTION' (str) - violation description associated with an establishment (restaurant) inspection
+                'CRITICAL_FLAG' (str) - indicator of critical violation
+                'SCORE' (float64) - total score for a particular inspection
+                'GRADE' (str) - grade associated with the inspection
+                'GRADE_DATE' (datetime64[ns]) - date when grade was issued to the establishment (restaurant)
+                'RECORD_DATE' (datetime64[ns]) - date record was added to dataset
+                'INSPECTION_TYPE' (str) - A combination of the inspection program and the type of inspection performed
+       
         """
         url = 'https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD'
         rest_ins_data = self.cache_file(url)
         df = pd.read_csv(rest_ins_data,
-                        parse_dates=['INSPECTION DATE', 'RECORD DATE'],
+                        parse_dates=['INSPECTION DATE', 'RECORD DATE', 'GRADE DATE'],
                         infer_datetime_format=True)
 
         df = format_data(df, from_date, to_date)
@@ -38,7 +71,7 @@ class RestaurantInspectionDataProvider(DataProvider):
         return build_network(df)
 
 
-    def format_data(self, df:pd.DataFrame, from_date, to_date):
+    def format_data(self, df: pd.DataFrame, from_date: str, to_date: str):
         """Format and clean the data."""
 
         # Drop values with unknown latitude and longitude
@@ -64,7 +97,7 @@ class RestaurantInspectionDataProvider(DataProvider):
         return df
 
 
-    def build_network(self, df:pd.DataFrame) -> network.SpatioTemporalNetwork:
+    def build_network(self, df: pd.DataFrame) -> network.SpatioTemporalNetwork:
         """Build an STTN network from filtered data."""
 
         # Separate features for edges and nodes
