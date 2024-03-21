84e52f205db8cc2b2887d117eb4f3248078a4460
Yuri Bogomolov <iura@tut.by>
2024-03-20 23:13:17 -0700

    Add combo arguments.

--- src/sttn/algorithms/community/detection.py
+++ src/sttn/algorithms/community/detection.py
@@ -6,9 +6,9 @@ import pycombo
 from sttn.network import SpatioTemporalNetwork
 
 
-def combo_communities(data: SpatioTemporalNetwork, weight: Optional[str] = None) -> SpatioTemporalNetwork:
+def combo_communities(data: SpatioTemporalNetwork, weight: Optional[str] = None, **kwargs) -> SpatioTemporalNetwork:
     graph = data.to_multigraph()
-    partition_dict, _ = pycombo.execute(graph, weight=weight)
+    partition_dict, _ = pycombo.execute(graph, weight=weight, **kwargs)
     partition_df = pd.DataFrame.from_dict(partition_dict, orient='index', columns=['cluster'])
     data_with_community = data.join_node_labels(partition_df)
     return data_with_community
