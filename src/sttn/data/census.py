c1c07e60f7672836217b03cc1ebfb1c2278e8d3c
BreckoD <43202795+BreckoD@users.noreply.github.com>
2024-05-27 23:51:52 +0200

    Update census.py to prevent C long OverflowError on Windows

--- src/sttn/data/census.py
+++ src/sttn/data/census.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import geopandas as gpd
-
+import numpy as np
 
 def get_state_codes() -> pd.DataFrame:
     url = 'https://www2.census.gov/geo/docs/reference/state.txt'
@@ -15,5 +15,5 @@ def get_tract_geo(state: str, year: int) -> gpd.GeoDataFrame:
     url = 'https://www2.census.gov/geo/tiger/TIGER{year}/TRACT/tl_{year}_{state_fips:02d}_tract.zip'.format(
         year=year, state_fips=fips_code)
     df = gpd.read_file(url)
-    df.GEOID = df.GEOID.astype(int)
+    df.GEOID = df.GEOID.astype(np.int64)  # np.int64 to fix windows C long issue
     return df
