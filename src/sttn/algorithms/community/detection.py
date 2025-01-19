d34bc4de9148ed648b88bc98f33ab30598141d1f
Daniel Bretsko <43202795+BreckoD@users.noreply.github.com>
2025-01-19 18:59:10 +0100

    Improving the system (#10)
    * Update data_analysis.j2
    
    Add an additional message to filter_nodes/edges method:"On input accepts a boolean pd.Series with the same index as the edges GeoDataFrame."
    
    * Update data_analysis.j2
    
    Added extended description for returning modularity in community detection
    
    * Update detection.py
    
    The function additionally returns the modularity score, set random_seed = 0 by default
    
    * Update nyc.py
    
    Changed description in get_data method to give more context
    
    * Update lehd.py
    
    Changed description in get_data method to give more context
    
    * Update data.py
    
    Added backoff on OpenAI's RateLimitError
    
    * Update data_analysis.j2
    
    Add a more straightforward explanation of `filter_nodes` and `filter_edges` and an example

--- src/sttn/algorithms/community/detection.py
+++ src/sttn/algorithms/community/detection.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 import pandas as pd
 import pycombo
@@ -6,9 +6,9 @@ import pycombo
 from sttn.network import SpatioTemporalNetwork
 
 
-def combo_communities(data: SpatioTemporalNetwork, weight: Optional[str] = None, **kwargs) -> SpatioTemporalNetwork:
+def combo_communities(data: SpatioTemporalNetwork, weight: Optional[str] = None, random_seed: Optional[int] = 0, **kwargs) -> Tuple[SpatioTemporalNetwork, float]:
     graph = data.to_multigraph()
-    partition_dict, _ = pycombo.execute(graph, weight=weight, **kwargs)
+    partition_dict, modularity = pycombo.execute(graph, weight=weight, random_seed=random_seed, **kwargs)
     partition_df = pd.DataFrame.from_dict(partition_dict, orient='index', columns=['cluster'])
     data_with_community = data.join_node_labels(partition_df)
-    return data_with_community
+    return data_with_community, modularity
