0866379c069f5eb9c0ae08b3ff8f5ff768a88118
Yuri Bogomolov <bogomoloviura@gmail.com>
2025-03-30 16:55:42 -0700

    Exclude data stats from the filtering prompt

--- src/sttn/nli/prompts.py
+++ src/sttn/nli/prompts.py
@@ -162,16 +162,16 @@ class PromptGenerator:
 
         data_provider = context.data_provider.__class__
         data_provider_instance = context.data_provider
-        nodes_description = PromptGenerator.get_df_description(context.network.nodes)
-        edges_description = PromptGenerator.get_df_description(context.network.edges)
+        # nodes_description = PromptGenerator.get_df_description(context.network.nodes)
+        # edges_description = PromptGenerator.get_df_description(context.network.edges)
 
         jcontext = {
             "user_query": context.query.query,
             "data_provider_documentation": data_provider.__doc__,
             "data_provider_arguments": context.data_provider_args,
             "data_description": data_provider_instance.get_data.__doc__,
-            "nodes_description": nodes_description,
-            "edges_description": edges_description
+            # "nodes_description": nodes_description,
+            # "edges_description": edges_description
         }
 
         prompt_str = template.render(jcontext)
